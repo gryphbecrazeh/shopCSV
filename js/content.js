77faa4553abb118cb3821d7b4cb0e0845fae50e7
Gryph <crcordine@gmail.com>
2019-01-26 16:54:16 -0500

    Added regex functionality, next will be adding sending messages between pop-up and content js files

--- js/content.js
+++ js/content.js
@@ -1,2 +1,5 @@
 let width = new RegExp('the');
 let body = document.getElementsByTagName("body");
+let addButton = document.getElementById("add");
+let the = /\w*/gmi;
+alert(document.body.innerText.match(the).length);
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-let addButton = document.getElementById("add");
-
 function addCart(){
     addButton.innerHTML="Added";
+
 }
 addButton.addEventListener("click",addCart);
